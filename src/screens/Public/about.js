342f1bec874eb6bc247b7bc8bd3fd6b2a385534f
Jacqueline Palmer <jackiemariep@yahoo.com>
2017-09-23 14:41:11 -0700

    Built About Page

---
+++ src/screens/Public/about.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Button, Grid, Header, Image, Segment } from 'semantic-ui-react';
+
+export default class AboutPage extends Component {
+  render() {
+    return (
+      <div>
+        <Segment style={{ padding: '8em 0em' }} vertical>
+          <Grid container stackable verticalAlign="middle">
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Header as="h3" style={{ fontSize: '2em' }}>
+                  You better "ECOnize"!
+                </Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  Econize encapsulates multiple image recognition services to
+                  better help conservationalists, researchers, and other fellow
+                  naturalists identify ecological data.
+                </p>
+                <Header as="h3" style={{ fontSize: '2em' }}>
+                  Identify Endangered Species in Your Area
+                </Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  Each image in ECOnize is matched against the International
+                  Union for Conservation of Nature's list of endangered species.
+                </p>
+              </Grid.Column>
+              <Grid.Column floated="right" width={6}>
+                <Image bordered rounded size="large" src="/elephants.jpeg" />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column textAlign="center">
+                <Button size="huge">Explore</Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </div>
+    );
+  }
+}
--- src/screens/Public/components/Menu.js
+++ src/screens/Public/components/Menu.js
@@ -3,14 +3,21 @@ import { Link } from 'react-router-dom';
 import { Menu, Header, Icon, Container } from 'semantic-ui-react';
 
 const Nav = () => (
-  <Menu borderless>
-    <Container>
-      <Menu.Item as={Link} to="/">
-        <Header style={{ fontWeight: 'bold', fontSize: '18px' }}>ECOnize</Header>
-      </Menu.Item>
+	<Menu borderless>
+		<Container>
+			<Menu.Item as={Link} to="/">
+				<Header style={{ fontWeight: 'bold', fontSize: '18px' }}>
+					ECOnize
+				</Header>
+			</Menu.Item>
+			<Menu.Item as={Link} to="/">
+				<Header style={{ fontWeight: '', fontSize: '18px' }}>
+					About
+				</Header>
+			</Menu.Item>
 
-      <Menu.Menu position="right" />
-    </Container>
-  </Menu>
+			<Menu.Menu position="right" />
+		</Container>
+	</Menu>
 );
 export default Nav;
